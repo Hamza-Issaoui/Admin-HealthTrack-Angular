f1cc0d09176afa0dc38c8f248137a4396340fd8c
Hamza Issaoui <haamza.el.issaoui@gmail.com>
2024-05-12 22:45:26 +0100

    add  AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { DefaultLayoutComponent } from './layout';
+import { AuthGuard } from './shared/authGard';
 
 export const routes: Routes = [
   {
@@ -9,6 +10,7 @@ export const routes: Routes = [
   },
   {
     path: '',
+    canActivate : [AuthGuard],
     component: DefaultLayoutComponent,
     data: {
       title: 'Home'
